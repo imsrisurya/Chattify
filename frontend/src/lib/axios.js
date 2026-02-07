da58ce83852d386fa672061e2034730f0775caf7
imsrisurya <srisuryadabbiru19@gmail.com>
2026-02-08 01:35:43 +0530

    fix: react version compatibility

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "/api";
+    : "https://chattify-k7rd.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
